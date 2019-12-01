90de0ce3517230d6af1932af87b8ff825d56936f
ruheni <aleruheni@outlook.com>
2019-12-01 18:23:23 +0300

    refactored navigation bar

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,35 +1,20 @@
 <template>
-  <!-- <b-navbar toggleable="lg" >
-    <b-nav vertical>
-      <b-nav-item to="/student">Coursework</b-nav-item>
-      <b-nav-item to="/student/grades">Grades</b-nav-item>
-      <b-nav-item to="/student/messages">Messages</b-nav-item>
-      <b-nav-item to="/student/account">My Account</b-nav-item>
-    </b-nav>
-  </b-navbar>-->
   <div>
-    <!-- <b-navbar toggleable="lg" type="light" variant="light">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
-            <template v-slot:button-content>
-              <strong>User</strong>
-            </template>
-            <b-dropdown-item to="/student/account">Profile</b-dropdown-item>
-            <b-dropdown-item href="/student/">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>-->
-    <b-navbar type="dark" variant="dark">
-      <b-navbar-brand to="/">NavBar</b-navbar-brand>
+    <b-navbar type="light" variant="light">
+      <b-navbar-brand to="#">Riara E-Learning</b-navbar-brand>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item v-if="username" @click="logout">
-          <em>Hello {{ username }}</em>
+        <!-- dropdown -->
+        <b-nav-item-dropdown text="Menu" right>
+          <b-dropdown-item to="/student/">Coursework</b-dropdown-item>
+          <b-dropdown-item to="/student/grades">Grades</b-dropdown-item>
+          <b-dropdown-item to="/student/messages">Messages</b-dropdown-item>
+          <b-dropdown-item to="/student/account">My Account</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <!--  -->
+        <b-nav-item v-if="username">
+          Hello
+          <em>{{ username }}</em>
         </b-nav-item>
         <b-nav-item v-if="$store.state.authUser" @click="logout">Logout</b-nav-item>
       </b-navbar-nav>
@@ -39,7 +24,11 @@
 <script>
 export default {
   name: 'Navbar',
-
+  data() {
+    return {
+      username: this.$store.state.authUser.username
+    }
+  },
   methods: {
     async logout() {
       try {
